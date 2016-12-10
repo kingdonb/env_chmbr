29c97fbb7eaab1923d2064343929fae80c523606
Kingdon Barrett <kingdon.b@nd.edu>
2016-12-10 21:58:24 +0000

    cucumber step definitions

---
+++ features/step_definitions/data_collector_steps.rb
@@ -0,0 +1,43 @@
+Given(/^I can read a JSON object from Micro\-controller Channel ID: (\d+)$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I get an empty response from the channel$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I can see an empty table for datapoints$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I record a null row into the database$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I get a data point with (\d+) values from the channel$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I record the data point into the database$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I get fifty data points at once$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^the data points are not already recorded in the database$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I record the data points into the database$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I can see data rows in the table for datapoints$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^the pre\-existing data rows in the database are preserved$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
